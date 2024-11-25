502dc7d92daeee310f3f08e45e92469ff7a77cb3
Md. Jahangir Alam Rohan <42867916+rohan9222@users.noreply.github.com>
2024-11-25 05:28:40 -0800

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 import './bootstrap';
+
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
+
+import jQueryUI from 'jquery-ui';
+window.jQueryUI = jQueryUI;
+
+import Popper from '@popperjs/core';
+window.Popper = Popper;
+
+import toastr from 'toastr';
+window.toastr = toastr;
+
+import Swal from 'sweetalert2';
+window.Swal = Swal;
+
+import ApexCharts from 'apexcharts';
+window.ApexCharts = ApexCharts;
+
+import Slick from 'slick-carousel';
+window.Slick = Slick;
+
+import moment from 'moment';
+window.moment = moment;
+
