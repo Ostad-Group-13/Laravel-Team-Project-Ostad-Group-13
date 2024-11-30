3fbc017f5c806039f06eb2cf4a44464e57cd4744
Md. Jahangir Alam Rohan <42867916+rohan9222@users.noreply.github.com>
2024-12-01 00:38:52 +0600

    Update 2024_11_27_093713_create_blogs_table.php

--- database/migrations/2024_11_27_093713_create_blogs_table.php
+++ database/migrations/2024_11_27_093713_create_blogs_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('slug');
+            $table->string('slug')->nullable();
             $table->text('short_description');
             $table->longText('long_description');
             $table->string('image')->nullable();
